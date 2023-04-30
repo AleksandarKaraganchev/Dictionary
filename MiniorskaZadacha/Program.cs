4ea361658f492b58509be6a726184930e481e797
AleksandarKaraganchev <aleksandar.karaganchev546@edu1.pgzaimov.com>
2023-04-30 08:20:40 +0300

    First commit

---
+++ MiniorskaZadacha/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniorskaZadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            string String = Console.ReadLine();
+            var resources = new Dictionary<string, int>();
+            while (String != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!(resources.ContainsKey(String))) //Какво прави този if: Ако изразът в скобите върне true, тогава на dictionary resources с key String се присвоява стойност 0.
+                {
+                    resources[String] = 0;
+                }
+                resources[String] = resources[String] + quantity;
+                String = Console.ReadLine();
+            }
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key}  --> {item.Value}");
+            }
+        }
+    }
+}
---
+++ TelefonenUkazatel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            var phonebook = new Dictionary<string, string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                string[] separator = input.Split();
+                if (separator[0] == "A")
+                {
+                    string name = separator[1];
+                    string phoneNum = separator[2];
+
+                    phonebook[name] = phoneNum;
+                }
+                if (separator[0] == "S")
+                {
+                    string name = separator[1];
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+            }
+        }
+    }
+}
